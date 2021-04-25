802d4a33b9e11662a1a7e7393ab36119a6b143cf
JAYA BHARGAVI VENGALA <bhargavivengala@gmail.com>
2021-04-24 21:05:56 -0700

    form js done

--- hw3/01-form.js
+++ hw3/01-form.js
@@ -1,3 +1,40 @@
 // Enter your code here
 
-console.log('Enter your code here');
+function Print() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var status = document.getElementById("status");
+    var section = document.getElementsByName("section");
+    var courses = document.getElementsByName("courses");
+    var comment = document.getElementById("comment");
+    var res = "";
+    console.log("===============Form Submission===============")
+    console.log("Name: "+ name.value);
+    console.log("Email: "+ email.value);
+    if (status.value =="Choose an Option") {
+        console.log("Class Registration: no selection");
+    } else {
+        console.log("Class Registration:"+status.value);
+    }
+    for(var i=0; i< section.length; i++) {
+        if(section[i].checked) {
+            if(section[i].value == "CS 410p")
+                console.log("Class section: Under Graduate");
+            if(section[i].value == "CS 510")
+                console.log("Class section: Graduate"); } 
+    }
+
+    for(var i=0; i< courses.length; i++) {
+        if(courses[i].checked) {
+            res += courses[i].value + ", ";
+        }
+    }
+    var printCourses = res.substring(0, res.length - 2);
+    console.log("Courses Taken: " + printCourses);
+    if (comment.value== null) {
+        console.log("Feedback: no feedback" + comment.value);
+  } else {
+    console.log("Feedback:"+comment.value);
+ }
+
+}
