7ecfc4b9ca6eb02c1ac83ebd5843ece5aa47e76b
JAYA BHARGAVI VENGALA <bhargavivengala@gmail.com>
2021-04-24 21:05:20 -0700

    background js done

--- hw3/05-background.js
+++ hw3/05-background.js
@@ -1,3 +1,30 @@
 // Enter your code here
 
-console.log('Enter your code here');
+function backgr(input) {
+    console.log(document.body.form)
+    if(input.value === "Stop") {
+        input.value = "Start";
+        input.style.backgroundColor = "blue";
+        document.getElementById("interval").disabled = false;
+        stop();
+    }
+    else if(input.value === "Start") {
+        input.value = "Stop";
+        input.style.backgroundColor = "red";
+        document.getElementById("interval").disabled = true;
+        start();
+    }
+}
+var interval;
+
+function start() {
+  var time = document.getElementById("interval").value;
+  interval = setInterval((time) => {
+    document.body.style.backgroundColor = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${(Math.random())})`;
+  }, (time*1000));
+}
+
+function stop() {
+  clearInterval(interval);
+}
+start();
