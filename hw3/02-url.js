cdbb1be722c02e92a41b8ecfe1e909b8d287d3d0
JAYA BHARGAVI VENGALA <bhargavivengala@gmail.com>
2021-04-24 20:59:01 -0700

    url js done

--- hw3/02-url.js
+++ hw3/02-url.js
@@ -1,6 +1,31 @@
 // Enter your code here
 
-console.log('Enter your code here');
+function manipulate() {
+    var temp = "";
+    let resultDiv = document.createElement("div");
+    resultDiv.className = "bg-light border rounded w-50 mx-auto mt-5 p-3";
+    var input = document.getElementById("comments").value;
+    var url = decodeURIComponent(input);
+    if(url.indexOf("?") === -1) {
+        let heading = document.createElement("div");
+        heading.innerHTML = ('<h1>Results</h1>'+'<h4>URL</h4>' +url);
+        resultDiv.append(heading);
+    } else {
+        var website = url.split("?");
+        let heading = document.createElement("div");
+        heading.innerHTML = ('<h1>Results</h1>'+'<h4>URL</h4>' + website[0]+ '<br><br>' + '<h4>Query Parameters</h4>');
+        resultDiv.append(heading);
+        var temp = website[1];
+        var params = temp.split("&");
+        params.forEach( element => {
+            var split = element.split("=");
+            let div = document.createElement("div");
+            div.innerHTML = split[0] + ": " + split[1];
+            resultDiv.append(div);
+        })
+    }
+    document.querySelector(".bg-info").append(resultDiv)
+}
 
 // Test Input #1:
 // http://www.example.com
