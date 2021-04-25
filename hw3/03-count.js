eaf57fde1edccb6d2a1a9de9698de434549b2f76
JAYA BHARGAVI VENGALA <bhargavivengala@gmail.com>
2021-04-24 23:32:04 -0700

    count js done

--- hw3/03-count.js
+++ hw3/03-count.js
@@ -1,3 +1,15 @@
 // Enter your code here
-
 console.log('Enter your code here');
+var input = document.getElementById('userinput');
+var output = document.getElementById('intro');
+function find() {
+    let reg = new RegExp("\\b" + input.value + "\\b","ig");
+    let inn = output.innerText.replace(reg, (same) => {
+        return `<span style="background-color:yellow;">${same}</span>`;
+    });
+    output.innerHTML = inn;
+}
+input.addEventListener('keyup', find, false);
+input.addEventListener('keydown', find, false);
+
+
